d2c9e19db33e3c5da390873655047bc6cfbcaf79
Jeremy Presutti <Jeremy@Presutti.us>
2021-10-25 19:15:20 -0400

    Version 2.0

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -22,15 +23,17 @@
 declare(strict_types=1);
 
 use Feast\Autoloader;
+use Feast\Interfaces\MainInterface;
 use Feast\Main;
+use Feast\ServiceContainer\ServiceContainer;
 
 // Application start time
 $startTime = microtime(true);
 
-define('APPLICATION_ROOT', __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR);
-define('CONTROLLERS_FOLDER', 'Controllers');
-define('HANDLERS_FOLDER', 'Handlers');
-define('PLUGINS_FOLDER', 'Plugins');
+const APPLICATION_ROOT = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR;
+const CONTROLLERS_FOLDER = 'Controllers';
+const HANDLERS_FOLDER = 'Handlers';
+const PLUGINS_FOLDER = 'Plugins';
 
 if (file_exists(APPLICATION_ROOT . 'maintenance.txt')) {
     http_response_code(503);
@@ -38,11 +41,11 @@ if (file_exists(APPLICATION_ROOT . 'maintenance.txt')) {
     include('maintenance-screen.html');
     exit;
 }
-if ( file_exists(APPLICATION_ROOT . 'vendor/autoload.php')) {
+if (file_exists(APPLICATION_ROOT . 'vendor/autoload.php')) {
     require_once(APPLICATION_ROOT . 'vendor/autoload.php');
-};
+}
 // Initialize autoloader
-if ( file_exists(APPLICATION_ROOT . '/Feast/Autoloader.php')) {
+if (file_exists(APPLICATION_ROOT . '/Feast/Autoloader.php')) {
     require_once(APPLICATION_ROOT . '/Feast/Autoloader.php');
 }
 $autoLoader = new Autoloader();
@@ -50,14 +53,14 @@ $autoLoader->register();
 
 $autoLoader->addPathMapping('Psr', ['/Feast/Psr']);
 
-define('RUN_AS', Main::RUN_AS_WEBAPP);
+const RUN_AS = Main::RUN_AS_WEBAPP;
 require_once(APPLICATION_ROOT . 'container.php');
-/** @var \Feast\ServiceContainer\ServiceContainer $container */
+/** @var ServiceContainer $container */
 $container->add(Autoloader::class, $autoLoader);
 
 // Turn on all error reporting and turn OFF display errors by default
 error_reporting(-1);
 ini_set('display_errors', 'false');
 
-$main = di(\Feast\Interfaces\MainInterface::class);
+$main = di(MainInterface::class);
 $main->main();
