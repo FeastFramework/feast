aeee4258f8bb9e54e1eacd0c4490c9bd2d0b7818
Jeremy Presutti <Jeremy@Presutti.us>
2021-11-25 14:03:46 -0500

    Update feast/framework to 2.0.0

--- Mapper/JobMapper.php
+++ Mapper/JobMapper.php
@@ -39,7 +39,7 @@ class JobMapper extends BaseMapper
     public function findOnePendingByQueues(array $queues): ?Job
     {
         $query = $this->getQueryBase()->where('status = ?', 'pending')->where(
-               'tries < max_tries and queue_name IN (' . str_repeat('?,', count($queues) - 1) . '?)',
+            'tries < max_tries and queue_name IN (' . str_repeat('?,', count($queues) - 1) . '?)',
             ...$queues
         );
         $return = $this->fetchOne($query);
@@ -50,7 +50,7 @@ class JobMapper extends BaseMapper
     }
 
     /**
-     * Mark Job as running.
+     * Mark job as running.
      *
      * @param \Model\Job $job
      * @return bool
@@ -61,11 +61,9 @@ class JobMapper extends BaseMapper
         $query = $this->connection->update(self::TABLE_NAME, ['status' => \Feast\Jobs\QueueableJob::JOB_STATUS_RUNNING])
             ->where(
                 'job_id = ? and status IN (?,?)',
-
                 $job->job_id,
                 \Feast\Jobs\QueueableJob::JOB_STATUS_PENDING,
                 \Feast\Jobs\QueueableJob::JOB_STATUS_FAILED
-
             );
         $result = $query->execute();
         return $result->rowCount() !== 0;
